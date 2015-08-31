6e56b57d12d1540acd8e712c82acef882c95877d
Levesque <levesqueca@biocomp-0-10.local>
2015-08-31 13:43:02 -0400

    Test 1

--- RstudioScript-25July2015.R
+++ RstudioScript-25July2015.R
@@ -9,6 +9,7 @@
 #check version installed
 ############################################################################################################################
 #Building the STAR Reference Genome Index:
+#test1
 cmd <- paste("mkdir", "GenomeDir")
 system(cmd)
 STAR_path <- "/home/AAFC-AAC/girouxem/RNASeq/tools/STAR-STAR_2.4.2a/source/STAR"
