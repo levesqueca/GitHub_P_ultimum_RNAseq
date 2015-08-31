055cc78bac69495ba458fc7adf8cfbd142ee3dc4
Emily Giroux <emily.giroux@agr.gc.ca>
2015-08-31 16:08:35 -0400

    Test 2 line 13

--- RstudioScript-25July2015.R
+++ RstudioScript-25July2015.R
@@ -10,6 +10,7 @@
 ############################################################################################################################
 #Building the STAR Reference Genome Index:
 #test 1 from girouxem
+# test 2 from emily
 cmd <- paste("mkdir", "GenomeDir")
 system(cmd)
 STAR_path <- "/home/AAFC-AAC/girouxem/RNASeq/tools/STAR-STAR_2.4.2a/source/STAR"
