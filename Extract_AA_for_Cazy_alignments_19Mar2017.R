12334911c0834717d0fe5629e12b22202eb5a3c2
Emily Giroux <emily.giroux@agr.gc.ca>
2017-06-28 11:40:09 -0400

    worked onit April 2017

--- Extract_AA_for_Cazy_alignments_19Mar2017.R
+++ Extract_AA_for_Cazy_alignments_19Mar2017.R
@@ -41,7 +41,7 @@ i <- 2
 #AA_fasta <- AAStringSet()
 rm(AA_fasta)
 for(i in 1:length(metadata_file2$ProteinsFasta)){
-  temp5 = list(readAAStringSet(paste(ref_transcriptomes_folder, metadata_file2$ProteinsFasta[i], ".gz", sep=""), 
+  temp5 = list(readAAStringSet(paste(ref_transcriptomes_folder, metadata_file2$ProteinsFasta[i], sep=""), 
                                             format="fasta"))
   if (i == 1) {
     AA_fasta <- temp5
@@ -60,14 +60,17 @@ for(i in 1:length(AA_fasta)){
 
 # Turn CAZy family names into factors in order to create
 # a list at the same time, in order to use indexing, use numbering: (cazy and species names)
-dbCAN_file2$family.hmm <- as.factor(dbCAN_file2$family.hmm)
+#dbCAN_file2$family.hmm <- as.factor(dbCAN_file2$family.hmm)
+
+dbCAN_tempFile$family.hmm <- as.factor(dbCAN_tempFile$family.hmm)
 
 species <- as.factor(metadata_file2$Species)
 
 levels(species)
 levels(species)[16] # ["Pythium_ultimum_var_ultimum"]
 
-dbCAN_file2$family.hmm[1]
+#dbCAN_file2$family.hmm[1]
+dbCAN_tempFile$family.hmm[1]
 
 factor(species)
 
@@ -75,10 +78,10 @@ which(species == "Pythium_ultimum_var_ultimum")
 
 i <- 1
 j <- 1
-dir.create(paste(dataPath, "/trimmedByCAZyDomain", sep = ""), showWarnings = TRUE, recursive = FALSE)
-trimmedFastaPath <- paste(dataPath, "/trimmedByCAZyDomain/", sep = "")
-for(i in 1:length(levels(dbCAN_file2$family.hmm))){
-newdata <- subset(dbCAN_file2, family.hmm == levels(dbCAN_file2$family.hmm)[i] )
+dir.create(paste(dataPath, "/trimmedByCAZyDomainAllCAZy", sep = ""), showWarnings = TRUE, recursive = FALSE)
+trimmedFastaPath <- paste(dataPath, "/trimmedByCAZyDomainAllCAZy/", sep = "")
+for(i in 1:length(levels(dbCAN_tempFile$family.hmm))){
+newdata <- subset(dbCAN_tempFile, family.hmm == levels(dbCAN_tempFile$family.hmm)[i] )
 rm(x_trim)
   for(j in 1:nrow(newdata)) {
       temp <- AAStringSet(
@@ -105,4 +108,41 @@ for(i in 1:length(ID_fasta_files)){
 }
 
 
+# The following is to generate a fasta per cazy family that includes sequences
+# for these families found inall our reference species. We did this a long time
+# ago before re-running the dbCAN cazy pipeline, and we did it only for the oomycetes
+# we had from Marcello's paper - so this will refresh and update that for us.
+# This way if I want to see the structure of a set of genes from a family of interest,
+# I'll be able to look at that and not be limited to only the family domain portions
+# when I want to check up on something in these sets.
+i <- 1
+j <- 1
+dir.create(paste(dataPath, "/subsetbyCAZyDomainAllCAZy", sep = ""), showWarnings = TRUE, recursive = FALSE)
+parsedFastaPath <- paste(dataPath, "/subsetbyCAZyDomainAllCAZy/", sep = "")
+for(i in 1:length(levels(dbCAN_tempFile$family.hmm))){
+  newdata <- subset(dbCAN_tempFile, family.hmm == levels(dbCAN_tempFile$family.hmm)[i] )
+  rm(x_trim)
+  for(j in 1:nrow(newdata)) {
+    temp <- AAStringSet(
+      AA_fasta[[which(species == newdata$Species[j])]][names(AA_fasta[[which(species == newdata$Species[j])]])==newdata$query.id[j]], 
+      width=NA, use.names=TRUE)
+    names(temp) <- paste(newdata$Species[j], names(temp), sep="_" )
+    if (j == 1) {
+      x_trim <- temp
+    } else {
+      x_trim <- c(x_trim,temp) }
+  }
+  writeXStringSet(x_trim, file=paste(parsedFastaPath, newdata$family.hmm[1],"_Parsed.fasta", sep=""), append=FALSE, format="fasta") 
+}
+
+
+ID_fasta_files <- list.files(path = parsedFastaPath, pattern = "\\.fas$|\\.fasta$", recursive = FALSE, full.names = FALSE)
+
+
+# makes a linsi command from this file without  reorientation
+for(i in 1:length(ID_fasta_files)){
+  cmd <- paste("/opt/bio/mafft/bin/mafft --reorder --quiet '",  trimmedFastaPath, ID_fasta_files[i], "' > ", 
+               trimmedFastaPath, "mafft_aligned_", ID_fasta_files[i], sep = "")
+  system(cmd)
+}
 
